44e4290b758b9d521d26c9290d79e1b13716da83
owner <owner@localhost>
2010-02-15 13:38:10 -0200

    (no commit message)

--- test/heat_test.py
+++ test/heat_test.py
@@ -13,10 +13,24 @@ class Paint(unittest.TestCase):
     def testDimension2(self):
         corner = 0.1715728752538099
         target = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        expected = [[corner, 0.25, corner], [0.25, 1.0, 0.25], [corner, 0.25, corner]]
+        expected = [[corner, 0.25, corner],
+                    [0.25, 1.0, 0.25],
+                    [corner, 0.25, corner]]
         point = [1, 1]
         intensity = 1
         heat.paint(target, point, intensity)
+        self.assertEqual(expected, target)
+    def testOverlay(self):
+        corner = 0.1715728752538099
+        target = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        expected = [[corner + 0.25, 1.25, corner + 0.25],
+                    [corner + 0.25, 1.25, corner + 0.25],
+                    [0.26706437806633621, 0.3611111111111111, 0.26706437806633621]]
+        point1 = [1, 1]
+        point2 = [0, 1]
+        intensity = 1
+        heat.paint(target, point1, intensity)
+        heat.paint(target, point2, intensity)
         self.assertEqual(expected, target)
 
 class WaveDispersion(unittest.TestCase):
