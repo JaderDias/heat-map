d5a22a0329e0fa7ea80023f4ba8f5802d2ebc083
owner <owner@localhost>
2010-02-17 02:06:39 -0200

    (no commit message)

--- heat_map/mercator_projection.py
+++ heat_map/mercator_projection.py
@@ -1,13 +1,12 @@
 import math
 
-degrees_to_radians_ratio = 180 / math.pi;
-radians_to_degrees_ratio = 1 / degrees_to_radians_ratio;
+radians_to_degrees = math.pi / 180
 
 def to_pixel(lat, lon, zoom, pixel_tile_size = 256):
     hemisphere_pixels = pixel_tile_size * math.pow(2, zoom - 1)
-    x_to_degrees_ratio = hemisphere_pixels / 180
-    y_to_radians_ratio = hemisphere_pixels / math.pi
-    x = hemisphere_pixels + (lon * x_to_degrees_ratio)
-    f = min(max(math.sin(lat * radians_to_degrees_ratio), -0.9999), 0.9999)
-    y = hemisphere_pixels + (.5 * math.log((1 + f) / (1 - f)) * -y_to_radians_ratio)
+    degrees_to_pixels = hemisphere_pixels / 180
+    radians_to_pixels = hemisphere_pixels / math.pi
+    x = hemisphere_pixels + (lon * degrees_to_pixels)
+    f = min(max(math.sin(lat * radians_to_degrees), -0.9999), 0.9999)
+    y = hemisphere_pixels + (.5 * math.log((1 + f) / (1 - f)) * -radians_to_pixels)
     return (x, y);
