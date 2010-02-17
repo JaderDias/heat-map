d90f14ccd654ee54d79fd2f324a712c4be670b45
owner <owner@localhost>
2010-02-17 21:29:38 -0200

    (no commit message)

--- heat_map/mercator_projection.py
+++ heat_map/mercator_projection.py
@@ -10,7 +10,7 @@ def __to_radians(degrees):
 def __to_degrees(radians):
     return radians * degrees_from_radians
 
-def __get_hemisphere(zoom, pixel_tile_size = 256.0):
+def __get_hemisphere(zoom, pixel_tile_size):
     return pixel_tile_size * math.pow(2.0, zoom - 1.0)
 
 def __to_pixel(semiarcs, hemisphere):
@@ -25,7 +25,7 @@ def __to_y(lat, hemisphere):
     lat_radians = -0.5 * math.log((1 + f) / (1 - f))
     return __to_pixel(lat_radians / math.pi, hemisphere)
 
-def to_pixel(lat, lon, zoom, pixel_tile_size = 256.0):
+def to_pixel(lat, lon, zoom, pixel_tile_size = 256):
     hemisphere = __get_hemisphere(zoom, pixel_tile_size)
     return (
             __to_x(lon, hemisphere),
@@ -39,13 +39,27 @@ def __to_lon(x, hemisphere):
     return __to_semiarcs(x, hemisphere) * 180.0
 
 def __to_lat(y, hemisphere):
-    y = __to_semiarcs(y, hemisphere) * math.pi
+    y = -__to_semiarcs(y, hemisphere) * math.pi
     return __to_degrees((2 * math.atan(math.exp(y))) - half_pi)
 
-def to_wgs84(x, y, zoom, pixel_tile_size = 256.0):
+def to_wgs84(x, y, zoom, pixel_tile_size = 256):
     hemisphere = __get_hemisphere(zoom, pixel_tile_size)
     return (
             __to_lat(y, hemisphere),
             __to_lon(x, hemisphere)
             )
 
+def __tile_to_pixel(tile, pixel_tile_size):
+    return tile * pixel_tile_size
+
+def get_extent(tile_x, tile_y, zoom, pixel_tile_size = 256):
+    top = __tile_to_pixel(tile_y, pixel_tile_size)
+    left = __tile_to_pixel(tile_x, pixel_tile_size)
+    bottom = __tile_to_pixel(tile_y + 1, pixel_tile_size)
+    right = __tile_to_pixel(tile_x + 1, pixel_tile_size)
+    north_east = to_wgs84(right, top, zoom, pixel_tile_size)
+    south_west = to_wgs84(left, bottom, zoom, pixel_tile_size)
+    return (
+            north_east,
+            south_west
+            )
--- test/mercator_projection_test.py
+++ test/mercator_projection_test.py
@@ -1,18 +1,63 @@
 import unittest
+from heat_map import matrix
 from heat_map import mercator_projection
+
+def assertFlattenAlmostEqual(test_case, expected, actual):
+    expected = matrix.flatten(expected)
+    actual = matrix.flatten(actual)
+    for item in zip(expected, actual):
+        test_case.assertAlmostEqual(item[0], item[1])
 
 class ToPixel(unittest.TestCase):
     def test(self):
-        input = [(0, 0, 0), (0, -90, 0), (0, 90, 1)]
-        expected = [(128, 128), (64, 128), (384, 256)]
+        input = [
+                 (0, 0, 0),
+                 (0, -90, 0),
+                 (0, 90, 1),
+                 (-85.0511287798066, 0, 1),
+                 (85.0511287798066, 0, 2),
+                ]
+        expected = [
+                    (128, 128),
+                    (64, 128),
+                    (384, 256),
+                    (256, 512),
+                    (512, 0),
+                   ]
         for test in zip(input, expected):
             actual = mercator_projection.to_pixel(test[0][0], test[0][1], test[0][2])
-            self.assertEqual(test[1], actual)
+            assertFlattenAlmostEqual(self, test[1], actual)
 
 class ToWGS84(unittest.TestCase):
     def test(self):
-        input = [(128, 128, 0), (64, 128, 0), (384, 256, 1)]
-        expected = [(0, 0), (0, -90), (0, 90)]
+        input = [
+                 (128, 128, 0),
+                 (64, 128, 0),
+                 (384, 256, 1),
+                 (256, 512, 1),
+                 (512, 0, 2),
+                ]
+        expected = [
+                    (0, 0),
+                    (0, -90),
+                    (0, 90),
+                    (-85.0511287798066, 0),
+                    (85.0511287798066, 0),
+                   ]
         for test in zip(input, expected):
             actual = mercator_projection.to_wgs84(test[0][0], test[0][1], test[0][2])
-            self.assertEqual(test[1], actual)
+            assertFlattenAlmostEqual(self, test[1], actual)
+
+class GetExtent(unittest.TestCase):
+    def test(self):
+        input = [
+                 (1, 1, 1),
+                 (0, 1, 2),
+                ]
+        expected = [
+                    ((0, 180), (-85.0511287798066, 0)),
+                    ((66.513260443111847, -90), (0, -180)),
+                   ]
+        for test in zip(input, expected):
+            actual = mercator_projection.get_extent(test[0][0], test[0][1], test[0][2])
+            assertFlattenAlmostEqual(self, test[1], actual)
