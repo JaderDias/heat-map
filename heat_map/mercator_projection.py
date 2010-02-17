0a4757d777224b5aa9db9b1012927aaa74a0027c
owner <owner@localhost>
2010-02-17 20:54:27 -0200

    added pixel to WGS84 conversion

--- heat_map/mercator_projection.py
+++ heat_map/mercator_projection.py
@@ -1,25 +1,51 @@
 import math
 
-radians_from_degrees = math.pi / 180
+radians_from_degrees = math.pi / 180.0
+degrees_from_radians = 1.0 / radians_from_degrees
+half_pi = math.pi / 2.0
 
 def __to_radians(degrees):
     return degrees * radians_from_degrees
 
-def __to_pixel(radians, hemisphere):
-    return hemisphere * (1 + (radians / math.pi))
+def __to_degrees(radians):
+    return radians * degrees_from_radians
 
-def __to_x(lon_radians, hemisphere):
-    return __to_pixel(lon_radians, hemisphere)
+def __get_hemisphere(zoom, pixel_tile_size = 256.0):
+    return pixel_tile_size * math.pow(2.0, zoom - 1.0)
 
-def __to_y(lat_radians, hemisphere):
+def __to_pixel(semiarcs, hemisphere):
+    return hemisphere * (1.0 + semiarcs)
+
+def __to_x(lon, hemisphere):
+    return __to_pixel(lon / 180.0, hemisphere)
+
+def __to_y(lat, hemisphere):
+    lat_radians = __to_radians(lat)
     f = min(max(math.sin(lat_radians), -0.9999), 0.9999)
-    y = -.5 * math.log((1 + f) / (1 - f))
-    return __to_pixel(y, hemisphere)
+    lat_radians = -0.5 * math.log((1 + f) / (1 - f))
+    return __to_pixel(lat_radians / math.pi, hemisphere)
 
-def to_pixel(lat, lon, zoom, pixel_tile_size = 256):
-    hemisphere = pixel_tile_size * math.pow(2, zoom - 1)
-    (lat_radians, lon_radians) = map(__to_radians, (lat, lon))
+def to_pixel(lat, lon, zoom, pixel_tile_size = 256.0):
+    hemisphere = __get_hemisphere(zoom, pixel_tile_size)
     return (
-            __to_x(lon_radians, hemisphere),
-            __to_y(lat_radians, hemisphere)
+            __to_x(lon, hemisphere),
+            __to_y(lat, hemisphere)
             )
+
+def __to_semiarcs(pixels, hemisphere):
+    return (pixels / hemisphere) - 1.0
+
+def __to_lon(x, hemisphere):
+    return __to_semiarcs(x, hemisphere) * 180.0
+
+def __to_lat(y, hemisphere):
+    y = __to_semiarcs(y, hemisphere) * math.pi
+    return __to_degrees((2 * math.atan(math.exp(y))) - half_pi)
+
+def to_wgs84(x, y, zoom, pixel_tile_size = 256.0):
+    hemisphere = __get_hemisphere(zoom, pixel_tile_size)
+    return (
+            __to_lat(y, hemisphere),
+            __to_lon(x, hemisphere)
+            )
+
--- test/mercator_projection_test.py
+++ test/mercator_projection_test.py
@@ -3,10 +3,16 @@ from heat_map import mercator_projection
 
 class ToPixel(unittest.TestCase):
     def test(self):
-        lat = [0, 0, 0]
-        lon = [0, -90, 90]
-        zoom = [0, 0, 1, 3]
-        expected = [(128, 128), (64, 128), (384, 256), (463, 777)]
-        for test in zip(lat, lon, zoom, expected):
-            actual = mercator_projection.to_pixel(test[0], test[1], test[2])
-            self.assertEqual(test[3], actual)
+        input = [(0, 0, 0), (0, -90, 0), (0, 90, 1)]
+        expected = [(128, 128), (64, 128), (384, 256)]
+        for test in zip(input, expected):
+            actual = mercator_projection.to_pixel(test[0][0], test[0][1], test[0][2])
+            self.assertEqual(test[1], actual)
+
+class ToWGS84(unittest.TestCase):
+    def test(self):
+        input = [(128, 128, 0), (64, 128, 0), (384, 256, 1)]
+        expected = [(0, 0), (0, -90), (0, 90)]
+        for test in zip(input, expected):
+            actual = mercator_projection.to_wgs84(test[0][0], test[0][1], test[0][2])
+            self.assertEqual(test[1], actual)
